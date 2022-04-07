4e1f6948c890384282debcb3f7e3b50c6652b40a
Nadav Inbar <nadavinbar8@gmail.com>
2022-04-07 15:51:34 +0300

    added descriptions for works

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -20,24 +20,32 @@ const Work = () => {
       pics: [LoRGuardian1, LoRGuardian2, LoRGuardian3],
       strArray: ['L', 'o', 'R', ' ', 'G', 'u', 'a', 'r', 'd', 'i', 'a', 'n'],
       site: 'https://www.lorguardian.com',
+      description: `Overlay Application for Riot’s card game Legends of Runeterra!
+        it enhance your gameplay experience, capabilities, and show you a more diverse view to support you in every stage of the game.`,
     },
     {
       name: 'Taskflow',
       pics: [Taskflow1, Taskflow2],
       strArray: ['T', 'a', 's', 'k', 'f', 'l', 'o', 'w'],
       site: 'https://taskflow-nadav-tom-oded.herokuapp.com',
+      description:
+        'Task Management Application that helps you stay organized and keep track on the project that you and your team are working on.',
     },
     {
       name: 'Bird Tracker',
       pics: [BirdTracker1],
       strArray: ['B', 'i', 'r', 'd', ' ', 'T', 'r', 'a', 'c', 'k', 'e', 'r'],
       site: 'https://birbstracker.netlify.app',
+      description:
+        'React Application that I implemented infinite scrolling in React',
     },
     {
       name: 'Simple Piano',
       pics: [ReactPiano],
       strArray: ['S', 'i', 'm', 'p', 'l', 'e', ' ', 'P', 'i', 'a', 'n', 'o'],
-      site: 'https://simplereactcarousel.netlify.app',
+      site: 'https://simplereactpiano.netlify.app',
+      description:
+        'React Piano Application that you can play notes with the keyboard keys',
     },
     {
       name: 'React Carousel',
@@ -58,7 +66,8 @@ const Work = () => {
         'e',
         'l',
       ],
-      site: 'https://simplereactpiano.netlify.app',
+      site: 'https://simplereactcarousel.netlify.app',
+      description: 'My implementation of Pictures Carousel using React',
     },
   ]);
 
@@ -83,13 +92,16 @@ const Work = () => {
             {works.map((work, idx) => (
               <li key={`${work.name}-${idx}`} className='work'>
                 <a href={work.site} target='_blank' rel='noreferrer'>
+                  <div className='description'>
+                    <h2 className='work-name'>{work.name}</h2>
+                    <p className='work-description'>{work.description}</p>
+                  </div>
                   <img
                     className='work-pic'
                     key={work.pics[idx]}
                     src={work.pics[0]}
                     alt={work.name}
                   />
-                  <h2>{work.name}</h2>
                 </a>
               </li>
             ))}
